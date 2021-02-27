0289005066e41821ed409d86a17f82f657d80f34
mrgh97 <719475327@qq.com>
2021-02-28 02:46:58 +0800

    check git

--- src/com/person/Main.java
+++ src/com/person/Main.java
@@ -1,7 +1,7 @@
 package com.person;
 
 public class Main {
-
+    private static String FileName = "E:\\coding\\dataset\\weibo\\weibo.txt";
     public static void main(String[] args) {
 	// write your code here
     }
